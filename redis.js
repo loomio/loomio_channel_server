dc172c75698edbf4bd4d83d897083d6983de229b
Robert Guthrie <rob@guthrie.nz>
2020-08-19 16:25:05 +1200

    support ENV's for redis config

--- redis.js
+++ redis.js
@@ -2,7 +2,7 @@
 
 let
   redis = require('redis'),
-  redisClient = redis.createClient({ port: 6379, host: 'localhost' })
+  redisClient = redis.createClient({ port: (process.env.REDIS_PORT || 6379), host: (process.env.REDIS_HOST || 'localhost') })
 
 redisClient.on("error", function(err) {
   console.error("redisClient", err)
