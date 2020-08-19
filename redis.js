1efa0c9603ead9d70c2201df3e1626f8cca50d7d
Robert Guthrie <rob@guthrie.nz>
2020-08-19 16:42:47 +1200

    add console.log redis host

--- redis.js
+++ redis.js
@@ -4,6 +4,9 @@ let
   redis = require('redis'),
   redisClient = redis.createClient({ port: parseInt(process.env.REDIS_PORT || 6379), host: (process.env.REDIS_HOST || 'localhost') })
 
+console.log({ port: parseInt(process.env.REDIS_PORT || 6379), host: (process.env.REDIS_HOST || 'localhost') })
+
+
 redisClient.on("error", function(err) {
   console.error("redisClient", err)
 });
