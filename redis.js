216e96ff27bc598b9d886c22bd7a2b57a5083684
Robert Guthrie <rob@guthrie.nz>
2020-08-19 16:25:47 +1200

    parseInt port

--- redis.js
+++ redis.js
@@ -2,7 +2,7 @@
 
 let
   redis = require('redis'),
-  redisClient = redis.createClient({ port: (process.env.REDIS_PORT || 6379), host: (process.env.REDIS_HOST || 'localhost') })
+  redisClient = redis.createClient({ port: parseInt(process.env.REDIS_PORT || 6379), host: (process.env.REDIS_HOST || 'localhost') })
 
 redisClient.on("error", function(err) {
   console.error("redisClient", err)
