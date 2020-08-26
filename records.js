fb6a90c2f502c70cbf5a7ec6a24696fc57cdd2ae
Robert Guthrie <rob@guthrie.nz>
2020-08-26 13:19:02 +1200

    cool

--- records.js
+++ records.js
@@ -7,7 +7,7 @@ const redisSubscribe = redis.createClient(config.redis)
 const redisClient = redis.createClient(config.redis)
 
 const getAsync = promisify(redisClient.get).bind(redisClient);
-const zrangeAsync = promisify(redisClient.zrange).bind(redisClient);
+const zrangeAsync = promisify(redisClient.zrangebyscore).bind(redisClient);
 
 var records = async function(socket) {
   const recordsPath = socket.nsp.name
@@ -27,8 +27,8 @@ var records = async function(socket) {
     console.log("catchup:", {data: data, callback: callback})
     Object.keys(data).forEach(async (room) => {
       let clientScore = data[room]
-      records = await zrangeAsync("/records/"+room, clientScore, -1)
-      // console.log("fetching data", room, data[room], clientScore, records)
+      records = await zrangeAsync("/records/"+room, clientScore, "+inf")
+      // console.log("fetching data", room, data[room], records)
       callback(records.map(JSON.parse))
     })
   })
